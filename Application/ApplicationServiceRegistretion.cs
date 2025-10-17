fbc666a556f5441cf6da7bfeb048ee26c372cf04
gorkemetem <gorkemetem98@gmail.com>
2025-10-17 18:21:24 +0300

    Add CreateBrand feature with MediatR integration

---
+++ Application/ApplicationServiceRegistretion.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application;
+
+public static class ApplicationServiceRegistretion
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        // Add application services here (e.g., MediatR, AutoMapper, etc.)
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        return services;
+    }
+}
---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommand:IRequest<CreatedBrandResponse>
+{
+    public string Name { get; set; }
+
+
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+    {
+        public Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name
+            };
+
+            return null;
+        }
+    }
+}
